6335f0f5f7159ff6e66ef4c4de113ecb3f882124
albertofanini-prog <albertofanini@outlook.com>
2022-07-11 10:37:54 +0200

    inline styling with ternary operator

---
+++ 07_Dynamic-styling/app.js
@@ -0,0 +1,27 @@
+//1 - aggiungere active al div corrente
+
+const app = Vue.createApp({
+    data(){
+        return{
+            //2 - resettare gli active
+            boxAselected: false,
+            boxBselected: false,
+            boxCselected: false,
+        }
+    },
+    // 4 - Aggiungere method da usare quando il div è cliccato
+    methods: {
+        // 5 - dichiarare un identifier
+        boxSelected(box){
+            if (box === 'A'){
+                this.boxAselected = true;
+            } else if (box === 'B'){
+                this.boxBselected = true;
+            } else if (box === 'C'){
+                this.boxCselected = true;
+            }
+        }
+    }
+});
+
+app.mount('#styling');
