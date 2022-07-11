51e394a4e66014a10de0c80a012c4f0b23c3a2de
albertofanini-prog <albertofanini@outlook.com>
2022-07-11 14:17:37 +0200

    remove list items

--- 08_Rendering-conditional-content-and-lists/app.js
+++ 08_Rendering-conditional-content-and-lists/app.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
   data() {
     return{ 
-      goals: [],
+      goals: [
+        'Master in Vue!',
+        'A ToDoList',
+        'Here you can add items',
+        'Or removing it clicking on'
+      ],
       //3 - resettare valore di ingresso input 
       enteredGoalValue: ''
     };
@@ -11,6 +16,11 @@ const app = Vue.createApp({
     addGoal(){
       // 4 - inserire(push) qualsiasi cosa user scriva, nell'array
       this.goals.push(this.enteredGoalValue);
+    },
+    // 11 - Creare funzione per rimuovere un goal
+    removeGoal(index){
+      // 12 - Usare splice per irmuovere da array
+      this.goals.splice(index, 1);
     }
   }
 });
