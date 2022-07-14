ff85d756f84a4fc3e39af4eb83b9312ce9c947bc
albertofanini-prog <albertofanini@outlook.com>
2022-07-14 11:19:59 +0200

    import

--- 11_Vue-CLI/README.md
+++ 11_Vue-CLI/README.md
@@ -2,7 +2,5 @@
 Installare le dipendenze: npm install.
 Lanciare il server di lavoro: npm run serve
 
-
-ciao sono io
-
-ciao sono io di nuovo
+<!-- 2 -->
+Modificare main.js
--- 11_Vue-CLI/src/App.vue
+++ 11_Vue-CLI/src/App.vue
@@ -1,1 +1,29 @@
-this is vue app
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: '0',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: '1',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+</style>
