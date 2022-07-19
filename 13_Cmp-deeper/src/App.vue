d349c7af51c4c1ef3f1fe7db06b8130d752ff21a
albertofanini-prog <albertofanini@outlook.com>
2022-07-19 11:49:40 +0200

    setup

---
+++ 13_Cmp-deeper/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeUser: {
+        name: 'Alberto Fanini',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+}
+</style>
