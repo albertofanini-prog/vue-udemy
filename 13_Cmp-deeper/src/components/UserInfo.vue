57fb0f56cf7a7c1fb421a467fb08b4cebfbcf127
albertofanini-prog <albertofanini@outlook.com>
2022-07-19 18:01:33 +0200

    d

--- 13_Cmp-deeper/src/components/UserInfo.vue
+++ 13_Cmp-deeper/src/components/UserInfo.vue
@@ -1,31 +1,38 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <BaseCard>
+      <div class="user-info">
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </div>
+      <p>{{ infoText }}</p>
+    </BaseCard>
   </section>
 </template>
 
 <script>
+import BaseCard from './BaseCard.vue';
 export default {
-  props: ['fullName', 'infoText', 'role'],
+    props: ["fullName", "infoText", "role"],
+    components: { BaseCard }
 };
 </script>
 
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+<style lang="scss" scoped>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    .user-info{
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    p{
+      width: 100%;
+      text-align: left;
+    }
+  }
 </style>
