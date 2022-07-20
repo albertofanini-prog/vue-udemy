e2a68c9b639100f4a91c00c0b0db0fadca289f64
albertofanini-prog <albertofanini@outlook.com>
2022-07-20 11:12:42 +0200

    <teleport to=""/>

--- 14_Dynamic-components/src/components/ManageGoals.vue
+++ 14_Dynamic-components/src/components/ManageGoals.vue
@@ -3,10 +3,12 @@
         <h1>Manage goals</h1>
         <input type="text" ref="goal">
         <button @click="setGoal">Set goal</button>
-        <error-alert v-if="inputIsInvalid">
-            <h2>Input non è valido</h2>
-            <button @click="confirmError">Okay</button>
-        </error-alert>
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
+                <h2>Input non è valido</h2>
+                <button @click="confirmError">Okay</button>
+            </error-alert>  
+        </teleport>
     </div>
 </template>
 
