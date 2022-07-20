8732edc14222c1626291e45cec82506e7424df3e
albertofanini-prog <albertofanini@outlook.com>
2022-07-20 15:41:46 +0200

    button component, switch classes with props

---
+++ 15_Learning-resources-app/src/components/UI/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <button
+        :type="type"
+        :class="mode"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+    button {
+        padding: 5px 12px;
+        border-radius: 10px;
+        border: none;
+        background-color: #FF3737;
+        color: white;
+        cursor: pointer;
+    }
+
+    button:hover,
+    button:active {
+        background-color: #FF3737;
+    }
+
+    .flat {
+        background-color: transparent;
+        color: #3a0061;
+        border: none;
+    }
+
+    .flat:hover,
+    .flat:active {
+        background-color: #edd2ff;
+    }
+</style>
